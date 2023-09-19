978f04cadd46becb57ebb52b7b609188eb5a8b2e
vhdez <vhernandez@scienceleadership.org>
2023-09-19 14:53:44 -0400

    Create Teacher and Student objects.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Let's create an OOP model for SLA:");
+        Student student1 = new Student();
+        student1.name = "Andrew Sears";
 
         Teacher teacher1 = new Teacher();
         teacher1.name = "Mr. Latimer";
-        teacher1.subject = "Algebra 2";
-        // teacher1.student = ???;
         teacher1.greetStudents();
+        teacher1.subject = "Algebra 2";
+        teacher1.giveLesson();
+        teacher1.student = student1;
+
+        Teacher teacher2 = new Teacher();
+        teacher2.name = "Mr. Reddy";
+        teacher2.greetStudents();
+        teacher2.subject = "Pre-Calculus";
+        teacher2.giveLesson();
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,18 @@
 public class Student {
+    String name;
+    int grade;
+    boolean learnedEnough;
+    Assignment assignment;
+
+    void learn() {
+
+    }
+
+    void complain() {
+
+    }
+
+    void doHw() {
+
+    }
 }
